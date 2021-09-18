7effe05673cade15b03f30ff9bff8132cc145e90
Elnaz <e.pouranbarani@gmail.com>
2021-09-18 12:19:25 -0400

    gauge chart

--- chart.js
+++ chart.js
@@ -123,9 +123,52 @@ function buildCharts(sample) {
 
     //Use Plotly to plot the data with the layout.
       Plotly.newPlot("bubble",bubbleData,layout)
-    });
-    }
-  
+   
+
+       // Gauge Chart
+       // Create a variable that filters the metadata array for the object with the desired sample number.
+       // Create a variable that holds the first sample in the metadata array.
+      var metadata = data.metadata;
+      var resultArray2 = metadata.filter(sampleObj => sampleObj.id == sample)[0];
+      // console.log(metadata)
+      console.log(resultArray2)
+    // Create a variable that holds the washing frequency.
+       var washingFrequency=parseFloat(resultArray2.wfreq);
+       
+    // Create the trace for the gauge chart.
+    var gaugeData = [
+      {
+        domain:{x:[0,1],y:[0,1]},
+        value: washingFrequency,
+        title:{text:"<br>Belly Button Washing Frequency<br>Scrubs per Week"},
+        type:"indicator",
+        mode:"gauge+number",
+        gauge: {
+        axis: {range:[0,10]},
+          
+        steps: [
+            { range: [0, 2], color: "red"},
+            { range: [2, 4], color: "orange"},
+            { range: [4, 6], color: "yellow"},
+            { range: [6, 8], color: "limegreen"},
+            { range: [8, 10], color: "green"},
+          ],
+        bar: { color: "black"}
+        }
+        
+      }
+    ]; 
+
+  //Create the layout for the gauge chart.
+  var gaugeLayout={
+      autosize: true
+  };
+  //Use Plotly to plot the gauge data and layout.
+  Plotly.newPlot("gauge",gaugeData,gaugeLayout);
+  });
+}
+
+
 
 
  
